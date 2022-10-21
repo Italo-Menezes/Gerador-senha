9f397e5f29d006eabe1f81a2e9c432e5ba3b339e
Italo-Menezes <109984013+Italo-Menezes@users.noreply.github.com>
2022-10-21 11:46:34 -0300

    fiucionado

--- src/controllers/indexController.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const indexController = {
-  index: (req, res) => {
-    res.sendFile(path.join(__dirname,'../views/index.html'))
-  }
-} ;
-
-module.exports = indexController;
